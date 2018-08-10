b79367ddb34833799332ad1b61022bbbb241098e
Thom Stilwill <thom.stilwill@gmail.com>
2018-08-10 06:47:46 -0400

    new footer info

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  version = '1.0';
+  year = '2018';
+  message: string;
+
   constructor() { }
 
   ngOnInit() {
